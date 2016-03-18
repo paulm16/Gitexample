53d00d4540a7d93381139e2b08aa56b73d19da53
Machine 4 <gokuldasvattamala@gmail.com>
2016-03-18 12:52:55 +0530

    new command

--- app/src/main/java/com/example/machine3/myapplication/MainActivity.java
+++ app/src/main/java/com/example/machine3/myapplication/MainActivity.java
@@ -1,13 +1,8 @@
 package com.example.machine3.myapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 //This is main activity in Gitexample
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +16,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-   //new commands from vishnu and paul
+  //new command from gokul
 }
